9ba42ce52095b737ebf3d73a64d68d7e6e75a042
Harald Werner <harald.werner@codecentric.de>
2021-05-25 14:19:29 +0200

    add prettier and eslint to basics2

--- basics2-testing/src/app/base/base.component.ts
+++ basics2-testing/src/app/base/base.component.ts
@@ -1,20 +1,17 @@
-import { Component, OnInit } from "@angular/core";
-import { RandomService } from "../random.service";
+import { Component, OnInit } from '@angular/core';
+import { RandomService } from '../random.service';
 
 @Component({
-  selector: "app-base",
-  templateUrl: "./base.component.html",
-  styleUrls: ["./base.component.scss"]
+  selector: 'app-base',
+  templateUrl: './base.component.html',
+  styleUrls: ['./base.component.scss'],
 })
 export class BaseComponent implements OnInit {
-
   number = 0;
 
-  constructor(private readonly random: RandomService) {
-  }
+  constructor(private readonly random: RandomService) {}
 
   ngOnInit(): void {
     this.number = this.random.getRandomNumber();
   }
-
 }
